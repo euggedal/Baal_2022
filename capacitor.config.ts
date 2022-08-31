15521bb8eeffaea4f5eb2880ec6ece028081bd27
tron <euggedal@gmail.com>
2022-08-31 09:16:36 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Baal_2022',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
